efc3713a2f5ef354edab1a22e9f790e091ca4425
Le Dang Huy <huyg.a22052003@gmail.com>
2024-12-11 19:37:08 +0700

    update

--- Finally/obj/Debug/net8.0-windows/Finally.AssemblyInfo.cs
+++ Finally/obj/Debug/net8.0-windows/Finally.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finally")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e1c95fe65572139f77871b693fca4b501c125a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a633b3da3f8bed48d3fc082850be8ec7ce4a0d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finally")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finally")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
